118ad66fe6e5fc71b514d303ffaeb68df7a82efa
evgenykochergin <evgenykochergin@gmail.com>
2022-10-18 20:06:37 +0300

    Fix serialisation

--- src/main/java/com/evgenykochergin/calendar/json/Json.java
+++ src/main/java/com/evgenykochergin/calendar/json/Json.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.*;
 
@@ -15,6 +16,7 @@ public class Json {
 
     static {
         OBJECT_MAPPER.findAndRegisterModules();
+        OBJECT_MAPPER.registerModule(new Jdk8Module());
     }
 
     public static ObjectNode objectNode() {
